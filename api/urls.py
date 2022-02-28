febf8a9554178f7eb63b22540d2c811261458bf5
nana usui <nanausui.hl@gmail.com>
2022-02-28 07:06:36 -0500

    edit limit endpoint and show max, min data

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 # coding: utf-8
 from django.urls import path, include
-from .views import DataViewSet
+from .views import DataViewSet, LimitViewSet
 
 
 urlpatterns = [
     path('data/', DataViewSet.as_view()),
+    path('limit/', LimitViewSet.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,10 @@
-from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.generics import ListAPIView
 import django_filters.rest_framework
 
 from .models import User, Days, Months
 from django.db.models import Max, Min
 
-from .serializer import UserSerializer, DaysSerializer, MonthsSerializer
+from .serializer import DaysSerializer, MonthsSerializer, LimitSerializer
 
 
 class DataViewSet(ListAPIView):
@@ -34,4 +34,33 @@ class DataViewSet(ListAPIView):
 
         return self.serializer_class
 
+
+class LimitViewSet(ListAPIView):
+    queryset = User.objects.filter(id=1)
+    serializer_class = LimitSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+
+    def get_object(self):
+
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            'Expected view %s to be called with a URL keyword argument '
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            'attribute on the view correctly.' %
+            (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        print(obj)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
 
