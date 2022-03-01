f0afea9a1183b588e31d9b621c384c7688b06892
nana usui <nanausui.hl@gmail.com>
2022-03-01 03:30:11 -0500

    filtering data by request user id

--- api/serializer.py
+++ api/serializer.py
@@ -5,7 +5,7 @@ from .models import User, Days, Months
 class DaysSerializer(serializers.ModelSerializer):
     class Meta:
         model = Days
-        fields = ('')
+        fields = ('timestamp', 'consumption', 'temperature')
 
 
 class MonthsSerializer(serializers.ModelSerializer):
@@ -21,39 +21,45 @@ class LimitSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        max_days_data = Days.objects.filter(user_id = 1).order_by('consumption').last()
-        min_days_data = Days.objects.filter(user_id = 1).order_by('consumption').first()
-        max_months_data = Months.objects.filter(user_id = 1).order_by('consumption').last()
-        min_months_data = Months.objects.filter(user_id = 1).order_by('consumption').first()
+        
+        user = None
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            user = request.user
 
-        data["days"] = {
-            "timestamp": {
-            "minimum": min_days_data.timestamp,
-            "maximum": max_days_data.timestamp
-            },
-            "consumption": {
-            "minimum": min_days_data.consumption,
-            "maximum": max_days_data.consumption
-            },
-            "temperature": {
-            "minimum": min_days_data.temperature,
-            "maximum": max_days_data.temperature
-            },
-        }
+            max_days_data = Days.objects.filter(user_id = user.id).order_by('consumption').last()
+            min_days_data = Days.objects.filter(user_id = user.id).order_by('consumption').first()
+            max_months_data = Months.objects.filter(user_id = user.id).order_by('consumption').last()
+            min_months_data = Months.objects.filter(user_id = user.id).order_by('consumption').first()
 
-        data["months"] = {
-            "timestamp": {
-            "minimum": min_months_data.timestamp,
-            "maximum": max_months_data.timestamp
-            },
-            "consumption": {
-            "minimum": min_months_data.consumption,
-            "maximum": max_months_data.consumption
-            },
-            "temperature": {
-            "minimum": min_months_data.temperature,
-            "maximum": max_months_data.temperature
-            },
-        }
+            data["days"] = {
+                "timestamp": {
+                "minimum": min_days_data.timestamp,
+                "maximum": max_days_data.timestamp
+                },
+                "consumption": {
+                "minimum": min_days_data.consumption,
+                "maximum": max_days_data.consumption
+                },
+                "temperature": {
+                "minimum": min_days_data.temperature,
+                "maximum": max_days_data.temperature
+                },
+            }
+
+            data["months"] = {
+                "timestamp": {
+                "minimum": min_months_data.timestamp,
+                "maximum": max_months_data.timestamp
+                },
+                "consumption": {
+                "minimum": min_months_data.consumption,
+                "maximum": max_months_data.consumption
+                },
+                "temperature": {
+                "minimum": min_months_data.temperature,
+                "maximum": max_months_data.temperature
+                },
+            }
 
         return data
--- api/views.py
+++ api/views.py
@@ -8,25 +8,22 @@ from .serializer import DaysSerializer, MonthsSerializer, LimitSerializer
 
 
 class DataViewSet(ListAPIView):
-    queryset = Days.objects.filter(user_id=1)
+    queryset = None
     serializer_class = DaysSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
 
-
     def get_queryset(self):
-
         resolution = self.request.query_params.get('resolution')
         if resolution == "months":
-            queryset = Months.objects.filter(user_id=1)
+            queryset = Months.objects.filter(user_id=self.request.user.id)
         else:
-            queryset = Days.objects.filter(user_id=1)
+            queryset = Days.objects.filter(user_id=self.request.user.id)
 
         return queryset
 
 
     def get_serializer_class(self):
         resolution = self.request.query_params.get('resolution') 
-        
         if resolution == "months":
             self.serializer_class = MonthsSerializer
         else:
@@ -36,31 +33,10 @@ class DataViewSet(ListAPIView):
 
 
 class LimitViewSet(ListAPIView):
-    queryset = User.objects.filter(id=1)
+    queryset = None
     serializer_class = LimitSerializer
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
 
-    def get_object(self):
-
-        queryset = self.filter_queryset(self.get_queryset())
-
-        # Perform the lookup filtering.
-        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
-
-        assert lookup_url_kwarg in self.kwargs, (
-            'Expected view %s to be called with a URL keyword argument '
-            'named "%s". Fix your URL conf, or set the `.lookup_field` '
-            'attribute on the view correctly.' %
-            (self.__class__.__name__, lookup_url_kwarg)
-        )
-
-        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
-        obj = get_object_or_404(queryset, **filter_kwargs)
-
-        print(obj)
-
-        # May raise a permission denied
-        self.check_object_permissions(self.request, obj)
-
-        return obj
-
+    def get_queryset(self):
+        queryset = User.objects.filter(id=self.request.user.id)
+        return queryset
